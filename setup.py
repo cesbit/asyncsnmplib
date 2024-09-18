2f0d3cdfad648a68c6a26a22984ba444fa89e509
Koos85 <koosjoosten@gmail.com>
2024-09-18 10:16:37 +0200

    ci

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
